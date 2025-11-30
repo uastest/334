93cebafb9b9a16b53ba45097beba3d792891b336
uastest <banggian555@gmail.com>
2025-11-29 23:10:25 -0300

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -131,7 +131,7 @@ const LoginForm = ({ t, navigate, toggleForm }) => {
         {t('noAccount')}
         <button
           type="button"
-          onClick={toggleForm}
+          onClick={() => navigate('/#/register')}
           className="ml-2 font-bold text-blue-600 hover:text-blue-700 transition-colors"
         >
           {t('register')}
